b3ab44b6a94ceae21ca4dd387f339e919a36e290
rosa <91250433+rosa11130@users.noreply.github.com>
2021-09-30 17:40:59 +0800

    Add files via upload

---
+++ week03-3.cpp.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello %d World\n",300);
+    printf("%d %d %d\n", 300, 200, 100);
+}
---
+++ week03-4.cpp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+       int a;
+       printf("請輸入一個數字a:");
+
+       scanf("%d" , &a);
+       print("你剛剛輸入的是:%d",a);
+
+}
